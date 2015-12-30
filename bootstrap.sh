5feb10c5f3aee08ea3f1fb74e6ee8ab2f023f5e6
ebarras <ebarras@gmail.com>
2015-12-30 13:09:26 -0600

    Update bootstrap.sh
    Added some "What I think this does" comments.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
+	# What I think this does - Copies everything in the dotfiles folder other than the excluded to the user's home directory.
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
+# What I think this does - If you send the -f option it doesn't warn you that it will overwrite. If not, it asks first.
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
